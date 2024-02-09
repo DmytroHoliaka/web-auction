127ce7efe3aa1f125261b44df12e7897f5461df7
HoliakaDmytro <holidim39@gmail.com>
2024-02-09 13:07:50 +0200

    Entities: Add Auction entity

---
+++ WebAuction/Backend/Database/Entities/Auction.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAuction.Backend.Database.Entities
+{
+    public class Auction
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal StartPrice { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Guid CreatorId { get; set; }
+        public User? Creator { get; set; }
+
+        public ICollection<Bet>? Bets { get; set; }
+        public ICollection<User>? Users { get; set; }
+    }
+}
