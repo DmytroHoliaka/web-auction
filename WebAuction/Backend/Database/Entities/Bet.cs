41a8d89e991df89aa5481154e110ca815df8ac56
HoliakaDmytro <holidim39@gmail.com>
2024-02-09 13:07:55 +0200

    Entities: Add Bet entity

---
+++ WebAuction/Backend/Database/Entities/Bet.cs
@@ -0,0 +1,15 @@
+﻿namespace WebAuction.Backend.Database.Entities
+{
+    public class Bet
+    {
+        public Guid Id { get; set; }
+        public decimal Price { get; set; } 
+        public DateTime Time { get; set; }
+
+        public Guid UserId { get; set; }       
+        public User? User { get; set; }
+
+        public Guid AuctionId { get; set; }    
+        public Auction? Auction { get; set; }
+    }
+}
