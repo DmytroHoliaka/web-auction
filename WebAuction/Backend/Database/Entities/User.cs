40daf700ca7ee6023d74f6f60917377b954c386e
HoliakaDmytro <holidim39@gmail.com>
2024-02-09 13:08:06 +0200

    Entities: Add User entity

---
+++ WebAuction/Backend/Database/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAuction.Backend.Database.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Password { get; set; }  
+        
+        public ICollection<Auction>? CreatedAuctions { get; set; }
+        public ICollection<Bet>? Bets { get; set; }
+        public ICollection<Auction>? BetedAuctions { get; set; }
+    }
+}
