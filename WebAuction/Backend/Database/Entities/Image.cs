809f2d2329b80d7573f8f5695119d1eec12e285f
HoliakaDmytro <holidim39@gmail.com>
2024-02-09 13:08:01 +0200

    Entities: Add Image entity

---
+++ WebAuction/Backend/Database/Entities/Image.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAuction.Backend.Database.Entities
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public byte[]? Data { get; set; }
+        
+        public Guid AuctionId { get; set; } 
+        public Auction? Auction { get; set; }
+    }
+}
