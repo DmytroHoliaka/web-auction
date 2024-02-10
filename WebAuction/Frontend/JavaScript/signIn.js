a8ffb132a8747abb2ebf209d5d8070e5f9b9937e
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 17:08:48 +0200

    JavaScript: Add two options for returning after logging in

--- WebAuction/Frontend/JavaScript/signIn.js
+++ WebAuction/Frontend/JavaScript/signIn.js
@@ -1,19 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.log-in-button').addEventListener('click', function (e) {
         e.preventDefault();
+
+        function getQueryStringValue(key) {
+            return new URLSearchParams(window.location.search).get(key);
+        }
+
+        var returnUrl = getQueryStringValue('returnUrl');
+
+        if (returnUrl) {
+            document.getElementById('returnUrlField').value = returnUrl;
+        }
+        else {
+            document.getElementById('returnUrlField').value = "/";
+        }
+
         const form = document.getElementById('signInForm');
         const formData = new FormData(form);
 
         fetch(form.action, {
             method: 'POST',
             body: formData,
-            headers: {
-                'Accept': 'text/plain'
-            }
+            headers: { 'Accept': 'application/json' }
         })
-            .then(response => response.text())
+            .then(response => response.json()) 
             .then(data => {
-                document.getElementById('signInMessage').textContent = data;
+                if (data.success) {
+                    window.location.href = data.redirectUrl;
+                } else {
+                    document.getElementById('signInMessage').textContent = data.message;
+                }
             })
             .catch(error => console.error('Error:', error));
     });
