369c19acd8ba5abab24367f7533e0e059cca8d09
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 13:45:27 +0200

    Migrations: Update database

---
+++ WebAuction/Migrations/20240210114006_AddGetAuctionHistoryFunction.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAuction.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGetAuctionHistoryFunction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                CREATE OR ALTER FUNCTION GetAuctionHistory
+                (
+                	@AuctionId UNIQUEIDENTIFIER
+                )
+                RETURNS Table
+                AS
+                RETURN 
+                	SELECT 
+                		u.Username AS Username,
+                		b.Price AS Bid,
+                		b.[Date] AS [Date],
+                		b.Id AS BetId
+                	FROM 
+                		Bets AS b
+                	INNER JOIN Users AS u ON
+                		u.Id = b.UserId
+                	WHERE
+                		b.AuctionId = @AuctionId;
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS GetAuctionHistory;");
+        }
+    }
+}
