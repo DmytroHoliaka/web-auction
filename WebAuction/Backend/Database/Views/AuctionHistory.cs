e9b9abd24c3b28fb24f9012cb9422cedecb06250
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 13:43:44 +0200

    Views: Add view AuctionHistory

---
+++ WebAuction/Backend/Database/Views/AuctionHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAuction.Backend.Database.Views
+{
+    public class AuctionHistory
+    {
+        public string? Username { get; set; }
+        public decimal Bid { get; set; }
+        public DateTime Date { get; set; }
+        
+        public Guid BetId { get; set; }
+    }
+}
