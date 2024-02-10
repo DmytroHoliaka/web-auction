4579258fb92ad68e2dd82b5583a38238d0619807
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 09:53:50 +0200

    Validators: Optimize database validator

--- WebAuction/Backend/Validators/DatabaseValidator.cs
+++ WebAuction/Backend/Validators/DatabaseValidator.cs
@@ -13,39 +13,33 @@ namespace WebAuction.Backend.Validators
             _db = db;
         }
 
-        public async Task<bool> IsEmailExists(string? email)
+        public async Task<bool> IsEmailExists(string email)
         {
-            if (string.IsNullOrEmpty(email))
-            {
-                return false;
-            }
+            User? user = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+            return user is not null;
+        }
 
+        public async Task<bool> IsUsernameExists(string username)
+        {
+            User? user = await _db.Users.FirstOrDefaultAsync(u => u.Username == username);
+            return user is not null;
+        }
+
+        public async Task<string> GetUserSignInError(string email, string password)
+        {
             User? user = await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
             
-            return user is not null;
-        }
-
-        public async Task<bool> IsUsernameExists(string? username)
-        {
-            if (string.IsNullOrEmpty(username))
+            if (user is null)
             {
-                return false;
+                return "Such email doesn't exists";
             }
 
-            User? user = await _db.Users.FirstOrDefaultAsync(u => u.Username == username);
-
-            return user is not null;
-        }
-
-        public async Task<bool> IsPasswordSuitable(string email, string? password)
-        {
-            if (string.IsNullOrEmpty(password))
+            if (user.Password != password)
             {
-                return false;
+                return "Password is incorrect";
             }
 
-            User user = await _db.Users.FirstAsync(u => u.Email == email);
-            return user.Password == password;
+            return string.Empty;
         }
     }
 }
