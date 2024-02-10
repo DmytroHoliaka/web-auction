ddd8d9eb8495c162a4c36e101cf14364ebe71d6c
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 11:46:51 +0200

    Controllers: Add controller for sending AuctionSummary

--- WebAuction/Backend/Controllers/HomeController.cs
+++ WebAuction/Backend/Controllers/HomeController.cs
@@ -1,15 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAuction.Backend.Database.Context;
+using WebAuction.Backend.Database.Views;
 
 namespace WebAuction.Backend.Controllers
 {
     [Route("")]
     public class HomeController : Controller
     {
-        [HttpGet]
-        public IActionResult GetMainPage()
+        private readonly ApplicationContext _db;
+
+        public HomeController(ApplicationContext db)
         {
-            string content = System.IO.File.ReadAllText("Frontend/index.html");
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMainPage()
+        {
+            string content = await System.IO.File.ReadAllTextAsync("Frontend/index.html");
             return Content(content, "text/html");
+        }
+
+        [HttpGet("Data/AuctionSummaries")]
+        public async Task<IActionResult> GetAllAuctions()
+        {
+            List<AuctionSummary> auctions = await _db.AuctionSummaries.ToListAsync();
+            return Json(auctions);
         }
     }
 }
