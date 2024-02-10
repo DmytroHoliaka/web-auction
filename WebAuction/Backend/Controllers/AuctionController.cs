3c4a0cc9a6787c91d577fb1c5dc403b8d571e754
HoliakaDmytro <holidim39@gmail.com>
2024-02-10 17:11:16 +0200

    Controllers: Add initial version of auction controller

---
+++ WebAuction/Backend/Controllers/AuctionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAuction.Backend.Controllers
+{
+    [Route("")]
+    public class AuctionController : Controller
+    {
+        [HttpGet("create_auction.html")]
+        public async Task<IActionResult> CreateAuction()
+        {
+            if (Request.Cookies["userStatus"] != "user")
+            {
+                return Redirect("/sign_in.html?returnUrl=/create_auction.html");
+            }
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Frontend", "create_auction.html");
+            string content = await System.IO.File.ReadAllTextAsync(filePath);
+            return Content(content, "text/html; charset=utf-8");
+        }
+    }
+}
