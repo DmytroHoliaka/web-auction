e1149f73bbe2bd6e11dc1ba16da329e89ea12e0d
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:26:48 +0200

    Migration: Add ImageData to view AuctionSummary

--- WebAuction/Migrations/20240211104811_ModifyViewAuctionSummary.Designer.cs
+++ WebAuction/Migrations/20240211104811_ModifyViewAuctionSummary.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAuction.Backend.Database.Context;
 
@@ -11,9 +12,11 @@ using WebAuction.Backend.Database.Context;
 namespace WebAuction.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240211104811_ModifyViewAuctionSummary")]
+    partial class ModifyViewAuctionSummary
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAuction/Migrations/20240211104811_ModifyViewAuctionSummary.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAuction.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyViewAuctionSummary : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                 CREATE OR ALTER VIEW AuctionSummary (ImageData, ListingTitle, StartingBid, CurrentBid, AuctionEnds, AuctionId) AS
+ 
+                    	SELECT 
+                			i.[Data],
+                       		a.[Name],
+                       		a.StartPrice,
+                       		dbo.GetMaxAuctionBet(a.Id),
+                       		a.EndDate,
+                         a.Id
+                    	FROM 
+                       		Auctions AS a
+                		LEFT JOIN Images AS i ON
+                			i.AuctionId = a.Id
+                		WHERE i.IsMain = 1
+                 
+                 WITH CHECK OPTION
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW IF EXISTS AuctionSummary");
+        }
+    }
+}
