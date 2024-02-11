de7c5d3a60295c27232b401bc423d21f68e575a2
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 21:20:54 +0200

    Controllers: Add initial version of controller for creating auctions

--- WebAuction/Backend/Controllers/AuctionController.cs
+++ WebAuction/Backend/Controllers/AuctionController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using WebAuction.Backend.Database.Management;
+using WebAuction.Backend.RequestViews;
 
 namespace WebAuction.Backend.Controllers
 {
-    [Route("")]
+    [Route("Auction")]
     public class AuctionController : Controller
     {
-        [HttpGet("create_auction.html")]
-        public async Task<IActionResult> CreateAuction()
+        private readonly DatabaseManager _dm;
+
+        public AuctionController(DatabaseManager dm)
+        {
+            _dm = dm;
+        }
+
+        [HttpGet("/create_auction.html")]
+        public async Task<IActionResult> ReachPage()
         {
             if (Request.Cookies["userStatus"] != "user")
             {
@@ -16,6 +26,19 @@ namespace WebAuction.Backend.Controllers
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Frontend", "create_auction.html");
             string content = await System.IO.File.ReadAllTextAsync(filePath);
             return Content(content, "text/html; charset=utf-8");
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> CreateAuction([FromBody] AuctionView auction)
+        {
+            await _dm.InsertAuctionAsync(auction.Name,
+                                         auction.Description,
+                                         auction.StartPrice,
+                                         auction.StartDate,
+                                         auction.EndDate,
+                                         auction.CreatorId);
+
+            return Ok();
         }
     }
 }
