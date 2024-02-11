71a7a702ca9228a897b2a6564a038e4efe081033
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:19:37 +0200

    Views: Add AuctionBetContent for bet content

---
+++ WebAuction/Backend/Database/Views/AuctionBetContent.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAuction.Backend.Database.Views
+{
+    public class AuctionBetContent
+    {
+        public string? Title { get; set; }
+        public decimal StartBet { get; set; }
+        public DateTime Deadline { get; set; }
+        public string? Description { get; set; }
+        public byte[]? MainImage { get; set; }
+        public List<byte[]>? Images { get; set; }
+    }
+}
