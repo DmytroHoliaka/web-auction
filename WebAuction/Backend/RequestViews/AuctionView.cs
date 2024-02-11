dc74ba13168122f61be43631d80a0e1590570c9d
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 21:22:39 +0200

    RequestViews: Add view to parse JSON file for creating new auction

---
+++ WebAuction/Backend/RequestViews/AuctionView.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAuction.Backend.RequestViews
+{
+    public class AuctionView
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal StartPrice { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid CreatorId { get; set; }
+    }
+}
