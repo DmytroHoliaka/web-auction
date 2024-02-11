a2d26fb5f10ba140b19717fb3f5fe9f572ce6d3c
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:20:21 +0200

    Controllers: Change controller auction and bet HTTP-query

--- WebAuction/Backend/Controllers/BetController.cs
+++ WebAuction/Backend/Controllers/BetController.cs
@@ -14,11 +14,59 @@ namespace WebAuction.Backend.Controllers
             _dm = dm;
         }
 
-        [HttpGet("{auctionId}")]
-        public async Task<IActionResult> GetHistory(Guid auctionId)
+        [HttpGet("Content")]
+        public async Task<IActionResult> GetAuctionDetails()
         {
+            string userStatus = Request.Cookies["userStatus"]!;
+
+            if (userStatus == "guest")
+            {
+                return Json(new { redirectToSignIn = true });
+            }
+
+            string? auctionIdStr = Request.Query["auctionId"];
+
+            if (auctionIdStr is null)
+            {
+                return BadRequest("Query string 'auctionId' is missing or empty.");
+            }
+
+            if (Guid.TryParse(auctionIdStr, out Guid auctionId) == false)
+            {
+                return BadRequest("Query string 'auctionId' is invalid.");
+            }
+
             List<AuctionHistory> history = await _dm.GetAuctionHistory(auctionId);
-            return Json(history);
+            AuctionBetContent auctionContent = await _dm.GetAuctionBetContent(auctionId);
+
+            var auctionDetails = new
+            {
+                Title = auctionContent.Title,
+                StartBet = auctionContent.StartBet,
+                Deadline = auctionContent.Deadline,
+                Description = auctionContent.Description,
+                MainImage = auctionContent.MainImage,
+                Images = auctionContent.Images,
+                History = history,
+            };
+
+            return Json(auctionDetails);
+        }
+
+        [HttpPost("Form")]
+        public async Task<IActionResult> MakeBet([FromBody] BidRequest bidRequest)
+        {
+            string error = _dm.GetBidValueError(bidRequest.Bid, bidRequest.AuctionId);
+            
+            if (error != string.Empty)
+            {
+                return Content(error);
+            }
+
+            await _dm.CreateBetAsync(bidRequest.Bid, bidRequest.Date, 
+                                     bidRequest.UserId, bidRequest.AuctionId);
+
+            return Content("Your bet was successfully added");
         }
     }
 }
