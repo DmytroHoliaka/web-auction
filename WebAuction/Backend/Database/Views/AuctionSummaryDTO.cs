c5a4335717c66002e12daa6beb504282017d17f2
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:19:05 +0200

    Views: Add AuctionSummaryDTO

---
+++ WebAuction/Backend/Database/Views/AuctionSummaryDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAuction.Backend.Database.Views
+{
+    public class AuctionSummaryDTO
+    {
+        public string? Base64Image { get; set; }
+        public string? ListingTitle { get; set; }
+        public decimal StartingBid { get; set; }
+        public decimal CurrentBid { get; set; }
+        public DateTime AuctionEnds { get; set; }
+        public Guid AuctionId { get; set; }
+    }
+}
