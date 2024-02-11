e29dcf9293d2f4362bd3c6a70d78947a70a4e0b1
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:19:19 +0200

    Views: Add BidRequest for JSON parsing

---
+++ WebAuction/Backend/Database/Views/BidRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAuction.Backend.Database.Views
+{
+    public class BidRequest
+    {
+        public decimal Bid { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserId { get; set; }
+        public Guid AuctionId { get; set; }
+    }
+}
