18f6f70ca987594786689a07f833000a0cccafcc
HoliakaDmytro <holidim39@gmail.com>
2024-02-11 19:18:49 +0200

    Views: Add Base64Image for image representation

--- WebAuction/Backend/Database/Views/AuctionSummary.cs
+++ WebAuction/Backend/Database/Views/AuctionSummary.cs
@@ -2,11 +2,16 @@
 {
     public class AuctionSummary
     {
+        public byte[]? ImageData { get; set; }
         public string? ListingTitle { get; set; }
         public decimal StartingBid { get; set; }
         public decimal CurrentBid { get; set; }
         public DateTime AuctionEnds { get; set; }
-        
         public Guid AuctionId { get; set; }
+
+        public string? Base64Image
+        {
+            get { return ImageData != null ? Convert.ToBase64String(ImageData) : null; }
+        }
     }
 }
